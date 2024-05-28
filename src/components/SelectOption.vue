b7a41279935f8cd5fbe5d84fac5756a74151f615
Gabriel Vanz <gabriel.vanz.silva@gmail.com>
2024-05-27 21:46:45 -0300

    [TSK-3] - feature: Sort and View Options
    Created the sort option on the HomeView, for the products
    Created the view options to delimit the items to show
    Created the SelectOption component
    Created the productService for HTTPS requests involving the products and for project pattern
    Created the ProductOptions util, for the SortBy and options array

---
+++ src/components/SelectOption.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{ options: { value: string; label: string }[] }>()
+const model = defineModel()
+</script>
+
+<template>
+  <select
+    v-model="model"
+    class="p-2 border border-gray-300 rounded-md focus-within:border-blue-300 focus-within:ring-2 focus-within:ring-blue-100 outline-none"
+  >
+    <option v-for="option in props.options" :key="option.value" :value="option.value">
+      {{ option.label }}
+    </option>
+  </select>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
 import type { Product } from '@/utils/Product'
-import { onMounted, ref } from 'vue'
-import axiosConfig from '@/services/axiosConfiguration'
-import HomeHeader from '@/components/HomeHeader.vue'
+import { onMounted, ref, watch } from 'vue'
 import ProductCard from '@/components/ProductCard.vue'
+import SelectOption from '@/components/SelectOption.vue'
+
+import { ArrowDownUp } from 'lucide-vue-next'
+import { getProducts } from '@/services/productService'
+import { options, type SortBy } from '@/utils/ProductOptions'
 
 const products = ref<Product[]>([])
+const sortBy = ref<SortBy>('asc')
+const selectedOption = ref<string>('')
 
 onMounted(() => {
-  axiosConfig.get('/products').then(res => {
-    products.value = res.data
+  getProducts().then(response => {
+    products.value = response.data
+  })
+})
+
+const handleChangeSortBy = () => {
+  sortBy.value = sortBy.value === 'asc' ? 'desc' : 'asc'
+}
+
+watch([sortBy, selectedOption], () => {
+  getProducts(selectedOption.value, sortBy.value).then(response => {
+    products.value = response.data
   })
 })
 </script>
 
 <template>
-  <HomeHeader />
   <div class="mt-5">
     <h1 class="text-3xl font-bold text-center">Products</h1>
+    <hr class="w-1/6 mx-auto my-5" />
+    <div class="flex justify-end items-center mr-5 p-5">
+      <span class="mr-2">Displayed Items</span>
+      <SelectOption v-model="selectedOption" :options="options" />
+      <button @click="handleChangeSortBy" class="flex items-center space-x-2">
+        <ArrowDownUp class="h-6 w-6" />
+      </button>
+    </div>
+    <div class="flex justify-end items-center"></div>
     <div class="flex flex-col justify-center lg:flex-row lg:flex-wrap">
       <div
         class="flex box-border lg:w-1/5 m-5 p-5 bg-white rounded-lg shadow-lg items-center justify-center"
